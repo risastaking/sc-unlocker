257cc410442f9eddcc41bbf26faabab1fe48260e
Mark Dreyer <mark@risasoft.com>
2021-11-02 04:16:24 -0500

    fix build issue with wasm output, update snippet

--- unlocker/interaction/testnet.snippets.sh
+++ unlocker/interaction/testnet.snippets.sh
@@ -1,10 +1,16 @@
-ALICE="${USERS}/alice.pem"
+CAROL="${USERS}/carol.pem"
 ADDRESS=$(erdpy data load --key=address-testnet)
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-testnet)
+FROM_TOKEN=0x4c4b4d45582d636661313364 # LKMEX-cfa13d
+TO_TOKEN=0x4d45582d313065373838 # MEX-10e788
+FEE_PERCENT=0x05dc # 1500 - 15%
 PROXY=https://testnet-api.elrond.com
 
 deploy() {
-    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=50000000 --arguments 0 --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=T || return
+    erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${CAROL} --gas-limit=50000000 \
+    --metadata-payable \
+    --arguments ${FROM_TOKEN} ${TO_TOKEN} ${FEE_PERCENT} \
+    --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=T || return
 
     TRANSACTION=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['emitted_tx']['hash']")
     ADDRESS=$(erdpy data parse --file="deploy-testnet.interaction.json" --expression="data['emitted_tx']['address']")
@@ -18,9 +24,10 @@ deploy() {
 
 add() {
     read -p "Enter number: " NUMBER
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=5000000 --function="add" --arguments ${NUMBER} --send --proxy=${PROXY} --chain=T
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${CAROL} --gas-limit=5000000 \
+     --function="add" --arguments ${NUMBER} --send --proxy=${PROXY} --chain=T
 }
 
-getSum() {
-    erdpy --verbose contract query ${ADDRESS} --function="getSum" --proxy=${PROXY}
+getFee() {
+    erdpy --verbose contract query ${ADDRESS} --function="getFee" --proxy=${PROXY}
 }
