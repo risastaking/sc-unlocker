bdb87b2844a3acdc10876ed1a8d7e835662fd81a
Mark Dreyer <mark@risasoft.com>
2021-11-01 22:02:47 -0500

    simplify implementation, add mandos tests

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,171 +1,114 @@
 #![no_std]
 
 elrond_wasm::imports!();
+const PERCENTAGE_TOTAL: u32 = 10_000; // 100%
 
 #[elrond_wasm::contract]
 pub trait Adder {
     #[proxy]
     fn self_proxy(&self) -> self::Proxy<Self::Api>;
 
-	#[init]
-	fn init(&self, from_token: TokenIdentifier, fee_percent: BigUint) -> SCResult<()>  {
-		let caller = self.blockchain().get_caller();
-		require!(!caller.is_zero(), "invalid caller");
-		require!(&fee_percent > &0, "no fee");
-
-		self.fee_percent().set(&fee_percent);
-		return self.add_from_token(from_token);
-	}
-
-	#[payable("*")]
-	#[endpoint(sendSc)]
-	fn send_sc(
-		&self,
-		#[payment_token] token_id: TokenIdentifier,
-		#[payment_nonce] nonce: u64,
-		#[payment_amount] amount: BigUint
-	) -> SCResult<BigUint>  {
-		require!(&amount > &0, "invalid amount");
-
-		self.send().direct(
-			&self.blockchain().get_sc_address(),
-			&token_id,
-			nonce,
-			&amount,
-			&[]);
-		Ok(amount)
-	}
-
-	#[payable("*")]
-	#[endpoint(sendMex)]
-	fn send_mex(
-		&self,
-        caller: &ManagedAddress,
-		amount: BigUint
-	) -> SCResult<()>  {
-		require!(!&caller.is_zero(), "invalid caller");
-
-		let fee_percent = self.fee_percent().get();
-		let amount_after_fee = &amount * &fee_percent / 100_u32;
-		let sc_balance = self.blockchain().get_sc_balance(&self.to_token().get(), 0);
-
-		require!(&amount_after_fee < &amount, "incorrect fee");
-		require!(&amount_after_fee <= &sc_balance, "no liquidity");
-		self.send().direct(
-			caller,
-			&self.to_token().get(),
-			0,
-			&amount_after_fee,
-			&[]);
-		Ok(())
-	}
-
-
-
-	#[payable("*")]
-	#[endpoint(swap)]
-	fn swap(
-		&self,
-		#[payment_token] token_id: TokenIdentifier,
-		#[payment_nonce] nonce: u64,
-		#[payment_amount] amount: BigUint
-	) -> SCResult<AsyncCall>   {
-		require!(amount >= 1, "minimum amount of 1 token");
-		require!(!self.blockchain().get_caller().is_zero(), "invalid caller");
-		require!(self.from_tokens().contains(&token_id), "token not supported");
-
-		Ok(self.self_proxy()
-				.contract(self.blockchain().get_sc_address())
-				.send_sc(token_id, nonce, amount)
-				.async_call()
-				.with_callback(
-					self.callbacks()
-				    .send_sc_callback(
-						&self.blockchain().get_caller())))
-	}
-
-    // callbacks
-
-    #[callback]
-    fn send_sc_callback(
+    #[init]
+    fn init(
         &self,
-        #[call_result] result: ManagedAsyncCallResult<BigUint>,
-        caller: &ManagedAddress
-    )  {
-        match result {
-            ManagedAsyncCallResult::Ok(amount) => {
-
-				self.self_proxy()
-				.contract(self.blockchain().get_sc_address())
-				.send_mex(caller, amount)
-				.async_call()
-				.with_callback(
-					self.callbacks()
-				    .send_mex_callback(
-						&caller));
-
-			self.last_error_message().clear();
-            },
-            ManagedAsyncCallResult::Err(message) => {
-				self.last_error_message().set(&message.err_msg);
-				//sc_error!("error sending LKMEX")
-            }
+        from_token: TokenIdentifier,
+        to_token: TokenIdentifier,
+        fee_percent: u32,
+    ) -> SCResult<()> {
+        let mut res = self.try_set_fee_percentage(fee_percent);
+        if (res.is_err()) {
+            return res;
         }
+
+        res = self.add_from_token(from_token);
+        if (res.is_err()) {
+            return res;
+        }
+
+        self.add_to_token(to_token)
     }
 
-	#[callback]
-    fn send_mex_callback(
+    #[payable("*")]
+    #[endpoint(swap)]
+    fn swap(
         &self,
-        #[call_result] result: ManagedAsyncCallResult<BigUint>,
-        caller: &ManagedAddress
-    )  {
-        match result {
-            ManagedAsyncCallResult::Ok(amount) => {
-			self.last_error_message().clear();
-            },
-            ManagedAsyncCallResult::Err(message) => {
-                //TODO: return the LKMEX
+        #[payment_token] token_id: TokenIdentifier,
+        #[payment_amount] amount: BigUint,
+    ) -> SCResult<()> {
+        require!(amount >= PERCENTAGE_TOTAL, "amount too small");
+        require!(!self.blockchain().get_caller().is_zero(), "invalid caller");
+        require!(
+            self.from_tokens().contains(&token_id),
+            "token not supported"
+        );
 
-				self.last_error_message().set(&message.err_msg);
-            }
-        }
+        let fee_percent = self.fee_percent().get();
+
+        require!(&fee_percent > &0, "zero fee");
+        let fee = self.calculate_percentage(&amount, &fee_percent);
+        let amount_after_fee = &amount - &fee;
+        let sc_balance = self.get_balance();
+
+        require!(&amount_after_fee < &amount, "incorrect fee");
+        require!(&amount_after_fee <= &sc_balance, "no liquidity");
+        require!(&amount_after_fee > &0, "nothing to send");
+        self.send().direct(
+            &self.blockchain().get_caller(),
+            &self.to_token().get(),
+            0,
+            &amount_after_fee,
+            &[],
+        );
+        Ok(())
     }
 
+    fn calculate_percentage(&self, total_amount: &BigUint, percentage: &BigUint) -> BigUint {
+        total_amount * percentage / PERCENTAGE_TOTAL
+    }
 
-	#[only_owner]
-	#[endpoint(addFromToken)]
-	fn add_from_token(&self, asset: TokenIdentifier) -> SCResult<()>{
-		require!(asset.is_valid_esdt_identifier(), "Invalid ESDT");
-		self.from_tokens().insert(asset);
-		Ok(())
-	}
+    #[only_owner]
+    #[endpoint(addFromToken)]
+    fn add_from_token(&self, asset: TokenIdentifier) -> SCResult<()> {
+        require!(asset.is_valid_esdt_identifier(), "Invalid ESDT");
+        self.from_tokens().insert(asset);
+        Ok(())
+    }
 
-	#[only_owner]
-	#[endpoint(setToToken)]
-	fn add_to_token(&self, asset: TokenIdentifier) -> SCResult<()>{
-		require!(asset.is_valid_esdt_identifier(), "Invalid ESDT");
-		self.to_token().set(&asset);
-		Ok(())
-	}
+    #[only_owner]
+    #[endpoint(setToToken)]
+    fn add_to_token(&self, asset: TokenIdentifier) -> SCResult<()> {
+        require!(asset.is_valid_esdt_identifier(), "Invalid ESDT");
+        self.to_token().set(&asset);
+        Ok(())
+    }
 
-	// STORAGE
-	#[view(getFee)]
-	#[storage_mapper("fee_percent")]
+    #[only_owner]
+    #[endpoint(setFee)]
+    fn try_set_fee_percentage(&self, new_fee_percentage: u32) -> SCResult<()> {
+        require!(
+            new_fee_percentage > 0 && new_fee_percentage < PERCENTAGE_TOTAL,
+            "Invalid percentage value, should be between 0 and 10,000"
+        );
+
+        self.fee_percent().set(&BigUint::from(new_fee_percentage));
+
+        Ok(())
+    }
+
+    // STORAGE
+    #[view(getFee)]
+    #[storage_mapper("fee_percent")]
     fn fee_percent(&self) -> SingleValueMapper<BigUint>;
 
-	#[storage_mapper("to_token")]
+    #[storage_mapper("to_token")]
     fn to_token(&self) -> SingleValueMapper<TokenIdentifier>;
 
-	#[view(getFromTokens)]
-	#[storage_mapper("from_tokens")]
-	fn from_tokens(&self) -> SetMapper<TokenIdentifier>;
+    #[view(getFromTokens)]
+    #[storage_mapper("from_tokens")]
+    fn from_tokens(&self) -> SetMapper<TokenIdentifier>;
 
-	#[view(getLiquidityBalance)]
-	fn get_balance(&self) -> BigUint{
-		self.blockchain().get_sc_balance(&self.to_token().get(), 0)
-	}
-
-	#[view(getLastError)]
-	#[storage_mapper("lastErrorMessage")]
-    fn last_error_message(&self) -> SingleValueMapper<ManagedBuffer>;
+    #[view(getLiquidityBalance)]
+    fn get_balance(&self) -> BigUint {
+        self.blockchain().get_sc_balance(&self.to_token().get(), 0)
+    }
 }
--- adder/tests/mandos_test.rs
+++ adder/tests/mandos_test.rs
@@ -17,6 +17,10 @@ fn adder_rs() {
     elrond_wasm_debug::mandos_rs("mandos/adder.scen.json", contract_map());
 }
 #[test]
+fn adder_deploy_validations_rs() {
+    elrond_wasm_debug::mandos_rs("mandos/adder-deploy-validations.scen.json", contract_map());
+}
+#[test]
 fn adder_swap_rs() {
     elrond_wasm_debug::mandos_rs("mandos/adder-swap.scen.json", contract_map());
 }
